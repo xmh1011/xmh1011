3928059ec370e6473c13e1b426b04f12c7eb3d90
xmh1011 <1197843839@qq.com>
2023-10-07 20:33:04 +0800

    feat: update README.md at 2023-10-07
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- script.sh
+++ script.sh
@@ -12,13 +12,11 @@ date=$(date +"%Y-%m-%d")
 
 line_number=$(($(wc -l < "$readme_path") - 4))
 
-title="- 🗓 Automatically updated by [script](./script.sh) at: "
+title="- 🗓 Updated at: "
 
 new_content="$title$now"
 
 sed -i '' "${line_number}s/.*/$new_content/" "$readme_path"
-
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
 cd $file_path
 
